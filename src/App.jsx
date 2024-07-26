2401fffafd6fc3ff7233b8e7987ac769fd44bb32
Xayida83 <lotta.lindberg83@gmail.com>
2024-07-26 15:54:41 +0200

    Services almost done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { Hero } from './Components/Hero/Hero'
 import { About } from './Components/About/About'
+import Services from './Components/Services/Services'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
-      
+      <Services/>
     </div>
   )
 }
