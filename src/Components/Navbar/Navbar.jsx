ef93edeaebaf9fbd0109ea32c6c6113adaf96809
Xayida83 <lotta.lindberg83@gmail.com>
2024-07-26 19:59:23 +0200

    line in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
+import underline from '../../assets/nav_underline.svg'
 
 const Navbar = () => {
+  
+    const [menu,setMenu] = useState("home");
   return (
     <div className='navbar'>
       <img src={logo} alt=""/> 
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+      <li><p onClick={()=>setMenu("home")}>Home</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+      <li><p onClick={()=>setMenu("about")}>About Me</p>{menu==="about"?<img src={underline}/>:<></>}</li>
+      <li><p onClick={()=>setMenu("services")}>Services</p>{menu==="services"?<img src={underline}/>:<></>}</li>
+      <li><p onClick={()=>setMenu("work")}>Portfolio</p>{menu==="work"?<img src={underline}/>:<></>}</li>
+      <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu==="contact"?<img src={underline}/>:<></>}</li>
       </ul>
       <div className="nav-connect">Connect With me</div>
     </div>
@@ -19,3 +22,28 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+// import React from 'react'
+// import './Navbar.css'
+// import logo from '../../assets/logo.svg'
+// import underline from '../../assets/nav_underline.svg'
+
+
+// const Navbar = () => {
+//   const [menu,setMenu] = useState("home");
+//   return (
+//     <div className='navbar'>
+//       <img src={logo} alt=""/> 
+//       <ul className="nav-menu">
+//         <li><p>Home</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+//         <li><p>About Me</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+//         <li><p>Services</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+//         <li><p>Portfolio</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+//         <li><p>Contact</p>{menu==="home"?<img src={underline}/>:<></>}</li>
+//       </ul>
+//       <div className="nav-connect">Connect With me</div>
+//     </div>
+//   )
+// }
+
+// export default Navbar
