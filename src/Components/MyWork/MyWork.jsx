1c2c39c1fe4ab95239c818a0fd05fd055cc893ff
Xayida83 <lotta.lindberg83@gmail.com>
2024-08-19 11:05:53 +0200

    more info on work

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -11,12 +11,29 @@ const MyWork = () => {
         <h2>My projects</h2>
         <img src={theme_pattern} alt="theme pattern" />
       </div>
-      <div className="mywork-container">
+      {/* <div className="mywork-container">
         {mywork_data.map((work,index)=>{
-          return <a key={index} href={work.w_link} target="_blank" rel="noopener noreferrer">
+          return 
+          <a key={index} href={work.w_link} target="_blank" rel="noopener noreferrer">
           <img src={work.w_img} alt="project image" />
-        </a>
-          // <img key={index} src={work.w_img} alt=""/>           
+        </a>        
+        })}
+      </div> */}
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <div key={index} className="project-card">
+              <div className="project-facts">
+                <h3 className="project-title">{work.w_name}</h3>
+                <p className="project-fact">Collaborator: {work.w_colab}</p>
+                <p className="project-fact">Period: {work.w_period}</p>
+                <p className="project-fact">{work.w_info}</p>
+              </div>
+              <a href={work.w_link} target="_blank" rel="noopener noreferrer">
+                <img src={work.w_img} alt={work.w_name} className="project-image" />
+              </a>
+            </div>
+          );
         })}
       </div>
       <a href="http://github.com/Xayida83" target="_blank" rel="noopener noreferrer">
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -9,37 +9,55 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Pokemon",
+        w_colab:"None",
+        w_period:"April 2024",
         w_img:project1_img,
+        w_info:"Using a Pokemon api to present and compare different Pokemons. Made with JavaScript, HTML and CSS",
         w_link:'https://pokegamebylotta.netlify.app/'
     },
     {
         w_no:2,
         w_name:"Wheels of time",
+        w_colab:"None",
+        w_period:"March 2024",
         w_img:project2_img,
+        w_info:"I embarked on an engaging project to develop an advanced Tamagotchi simulation. Made with JavaScript and CSS",
         w_link:'https://wheelsoftimecreatures.netlify.app/'
     },
     {
         w_no:3,
-        w_name:"Quiz",
+        w_name:"Ankademin-Quiz",
+        w_colab:"None",
+        w_period:"January 2023",
         w_img:project3_img,
+        w_info:"For the final grade in the JavaScript 1. Made with JavaScript, HTML and SASS",
         w_link:'https://quizforankademin.netlify.app/'
     },
     {
         w_no:4,
         w_name:"Rent a plant",
+        w_colab:"Hanna Rubensson, Gabriella Broberg",
+        w_period:"October 2023",
         w_img:project4_img,
+        w_info:"Part of a course on HTML, CSS, and Accessibility. Our goal was to design a responsive and accessible website",
         w_link:'https://rentaplant.netlify.app/'
     },
     {
         w_no:5,
         w_name:"Sunday Reset",
+        w_colab:"Hanna Rubensson, Gabriella Broberg",
+        w_period:"March 2024",
         w_img:project5_img,
+        w_info:"For an Agile development course. Made with JavaScript, HTML and SASS",
         w_link:'https://sundayreset.netlify.app/'
     },
     {
         w_no:6,
-        w_name:"Book Ducks",
+        w_name:"BookDucks",
+        w_colab:"None",
+        w_period:"May 2024",
         w_img:project6_img,
+        w_info:"Learning CMS Strapi. Made with JavaScript, HTML and SASS",
         w_link:'https://github.com/Xayida83/CMS_Projekt_lotta_lindberg'
     },
 ]
