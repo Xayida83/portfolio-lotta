8ca13d072e697f98ea64946ddfd42e6b85d0d900
Xayida83 <lotta.lindberg83@gmail.com>
2024-08-22 11:08:21 +0200

    changing text on hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,8 +8,8 @@ export const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={profile_img} alt="" />
-      <h1><span>I'm Charlotta Lindberg,</span> frontend developer based in Sweden</h1>
-      <p>I'm a student living in Stockholm excited about my new career as a developer.</p>
+      <h1><span>I'm Charlotta Lindberg,</span> frontend developer</h1>
+      <p>Graduated .NET developer with focus on frontend.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
         
