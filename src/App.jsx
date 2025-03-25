a2aa1b6bab3f60c739a710139962099389f58a0e
Xayida83 <lotta.lindberg83@gmail.com>
2025-03-25 09:42:14 +0100

    no fillout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Hero } from './Components/Hero/Hero'
 import { About } from './Components/About/About'
 // import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
-// import Contact from './Components/Contact/Contact'
+import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 
@@ -16,7 +16,7 @@ const App = () => {
       <About/>
       {/* <Services/> */}
       <MyWork/>
-      {/* <Contact/> */}
+      <Contact/>
       <Footer/>
       
     </>
